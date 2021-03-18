bf0aea7ef3a724837359c0a246f51e4eb646cbad
Martí Bolívar <marti.bolivar@nordicsemi.no>
2021-03-17 22:13:24 -0700

    edtlib: packaging tweaks
    Tweaks for importing now that we're inside a devicetree package.
    
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

--- src/devicetree/edtlib.py
+++ src/devicetree/edtlib.py
@@ -80,10 +80,10 @@ try:
 except ImportError:
     from yaml import Loader
 
-from dtlib import DT, DTError, to_num, to_nums, TYPE_EMPTY, TYPE_BYTES, \
-                  TYPE_NUM, TYPE_NUMS, TYPE_STRING, TYPE_STRINGS, \
-                  TYPE_PHANDLE, TYPE_PHANDLES, TYPE_PHANDLES_AND_NUMS
-from grutils import Graph
+from .dtlib import DT, DTError, to_num, to_nums, TYPE_EMPTY, TYPE_BYTES, \
+                   TYPE_NUM, TYPE_NUMS, TYPE_STRING, TYPE_STRINGS, \
+                   TYPE_PHANDLE, TYPE_PHANDLES, TYPE_PHANDLES_AND_NUMS
+from .grutils import Graph
 
 
 #
